f3eff6c0adc64240ede2d97a10e7af4c8c3f2f8e
Andrew Zahalski <info@zahalski.dev>
2024-09-15 08:54:28 +0300

    gulp checkpoint

---
+++ update/2.2.4/install/version.php
@@ -0,0 +1,6 @@
+<?
+$arModuleVersion = array(
+	"VERSION" => "2.2.4",
+	"VERSION_DATE" => "2024-08-30 13:05:00"
+);
+?>
---
+++ update/2.2.5/install/version.php
@@ -0,0 +1,6 @@
+<?
+$arModuleVersion = array(
+	"VERSION" => "2.2.5",
+	"VERSION_DATE" => "2024-09-15 08:28:00"
+);
+?>
